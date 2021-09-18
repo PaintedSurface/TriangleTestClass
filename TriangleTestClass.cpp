b4ed675a6b46b942b40d27c39f3c848a286116ff
Josh <Josh@DESKTOP-2RAA8HA>
2021-09-18 13:18:34 -0400

    Add project files.

---
+++ TriangleTestClass.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TriangleTestClass
+{
+	TEST_CLASS(TriangleTestClass)
+	{
+	public:
+		
+		TEST_METHOD(Hypotenuse)
+		{
+			//Arrange 
+			int sideA = 2;
+			int sideB = 3;
+			triangle testTriangle(); 
+
+	};
+}
+
+class triangle
+{
+	int calculateHypotenuse(int sideA, int sideB)
+	{
+
+		return 0; 
+	}
+};
